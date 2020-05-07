f12baaebd50efc36ce805be02a62b4cd96a10eb7
sagardesai90 <saagardesai@gmail.com>
2020-05-07 00:46:17 -0500

    deployed app to firebase, with code editor working

--- src/containers/pages/Home.js
+++ src/containers/pages/Home.js
@@ -1,16 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Header from "../../components/Header";
-import { database } from "firebase";
 import rand from "random-key";
-// import * as config from "../../firebase-config.json";
+import { database } from "firebase";
 
 export default class HomePage extends React.Component {
   state = {
     key: rand.generate(5),
     num: null,
   };
-
   componentDidMount = () => {
     database()
       .ref("code-sessions")
@@ -28,18 +26,21 @@ export default class HomePage extends React.Component {
       });
     this.props.history.push("/" + this.state.key);
   };
-
   render() {
     return (
       <React.Fragment>
-        <Header />
+        <Header
+          extras={this.state.num ? `Total ${this.state.num}+ Shares` : null}
+        />
         <div className="homepage">
           <p className="title">
+            <br />
             Share Code with in <span className="highlight">Realtime</span>.
             <br />
             Anywhere, Anytime and with <span className="highlight">Anyone</span>
             .
           </p>
+
           <p className="sub-title">
             Simple Realtime Code Sharing Editor App. Using Firebase Realtime
             Database and Code Mirror as Editor.
@@ -54,3 +55,6 @@ export default class HomePage extends React.Component {
     );
   }
 }
+
+// WEBPACK FOOTER //
+// ./src/containers/pages/Home.js
