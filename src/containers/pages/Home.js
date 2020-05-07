9fe16b806d0181723337cc4fe9df3c6feb63aaac
sagardesai90 <saagardesai@gmail.com>
2020-05-06 23:56:31 -0500

    first commit

---
+++ src/containers/pages/Home.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Header from "../../components/Header";
+import { database } from "firebase";
+import rand from "random-key";
+// import * as config from "../../firebase-config.json";
+
+export default class HomePage extends React.Component {
+  state = {
+    key: rand.generate(5),
+    num: null,
+  };
+
+  componentDidMount = () => {
+    database()
+      .ref("code-sessions")
+      .on("value", (s) => {
+        this.setState({ num: s.numChildren() });
+      });
+  };
+
+  onNewGround = () => {
+    database()
+      .ref("code-sessions/" + this.state.key)
+      .set({
+        content: "Happy Coding",
+        createdon: Date(),
+      });
+    this.props.history.push("/" + this.state.key);
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <div className="homepage">
+          <p className="title">
+            Share Code with in <span className="highlight">Realtime</span>.
+            <br />
+            Anywhere, Anytime and with <span className="highlight">Anyone</span>
+            .
+          </p>
+          <p className="sub-title">
+            Simple Realtime Code Sharing Editor App. Using Firebase Realtime
+            Database and Code Mirror as Editor.
+          </p>
+          <div>
+            <button className="btn" onClick={this.onNewGround}>
+              Share Code
+            </button>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
