a75ca0041d0e083f0808adf14a19dbc49763de1c
sagardesai90 <saagardesai@gmail.com>
2020-05-13 10:53:45 -0500

    language dropdown => onClick to onMouseOver

--- client/src/components/Dropdown.js
+++ client/src/components/Dropdown.js
@@ -29,7 +29,7 @@ class Dropdown extends React.Component {
     const { listOpen, headerTitle } = this.state;
     return (
       <div className="dd-wrapper">
-        <div className="dd-header" onClick={() => this.toggleList()}>
+        <div className="dd-header" onMouseOver={() => this.toggleList()}>
           <div className="dd-header-title">{headerTitle}</div>
           {listOpen ? (
             <FontAwesome name="angle-up" size="2x" />
