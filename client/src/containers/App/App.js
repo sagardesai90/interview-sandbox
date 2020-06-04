584053c2ab20bbe9af5029c6cc5b610719e260d5
sagardesai90 <saagardesai@gmail.com>
2020-06-03 19:14:48 -0500

    added crisp to the app

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -7,6 +7,21 @@ import HomePage from "../pages/Home";
 // import AppRouter from './App.router'
 
 class App extends Component {
+  componentDidMount() {
+    // Include the Crisp code here, without the <script></script> tags
+    window.$crisp = [];
+    window.CRISP_WEBSITE_ID = "8cf64773-bd33-4bfa-9b90-008d5d033135";
+
+    (function () {
+      var d = document;
+      var s = d.createElement("script");
+
+      s.src = "https://client.crisp.chat/l.js";
+      s.async = 1;
+      d.getElementsByTagName("head")[0].appendChild(s);
+    })();
+  }
+
   render() {
     return (
       <Router>
