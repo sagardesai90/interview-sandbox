4a067fc10e5745432de3cb2fac140d220de25d22
sagardesai90 <saagardesai@gmail.com>
2020-10-11 17:42:54 -0500

    logo

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import Chad from "./chad.png";
+import "./Header.css";
 type Props = {
   style: React.CSSProperties,
   extras: React.ReactHTML,
@@ -9,6 +10,7 @@ type Props = {
 const Header = (props: Props) => {
   return (
     <header style={props.style} className="App-header">
+      <img className="mainLogo" src={Chad} />
       <Link className="App-title" to="/">
         Interview Sandbox
       </Link>
